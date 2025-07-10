7fb1092bb7606ce6dddce539e6c1aef912c6cec2
pg9800 <garo91089@gmail.com>
2025-07-10 10:46:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function starRide(){
+  const pickup=
+ document.getELementById("pickup).value:
+   if(pickup){
+     alert("Your uber ride has been requested
+           from"+pickup+"!");
+          } else{
+alert("please enter a pickup location");
+          }
+}
